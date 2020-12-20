dc6aa989e5b3f8a264b6a0f927f41c6f38fb7bd4
YenLong Lee <YenLong_Lee@jabil.com>
2020-12-20 16:50:18 +0800

    add home-filter-body

--- src/app/modules/home/home-filter/home-filter.component.ts
+++ src/app/modules/home/home-filter/home-filter.component.ts
@@ -12,6 +12,7 @@ export class HomeFilterComponent implements OnInit {
   public searchPlaceholder: string;
   public searchKeywords: string;
   public isFilterVisible: boolean;
+  public filterProperties: FilterProperty[];
 
   private _searchInput: ElementRef;
   @ViewChild('searchInput', { static: false }) set content(content: ElementRef) {
@@ -26,10 +27,35 @@ export class HomeFilterComponent implements OnInit {
     this.searchPlaceholder = "tap here to search"
     this.searchKeywords = "";
     this.isFilterVisible = false;
+    this.filterProperties = [
+      new FilterProperty("distance", 10, "km"),
+      new FilterProperty("waiting time", 60, "min"),
+      new FilterProperty("rating", 1, "★")
+    ];
+    console.log(this.filterProperties);
   }
 
   showFilterBody() {
     this.isFilterVisible = true;
   }
 
+  hideFilterBody() {
+    this.isFilterVisible = false;
+  }
+
+  setFilterProperty(id: number, value: any, percent: number) {
+    this.filterProperties[id].value = value;
+  }
+}
+
+class FilterProperty {
+  public name: string;
+  public value: any;
+  public unit: string;
+
+  constructor(name: string, value: any = 0, unit: string) {
+    this.name = name;
+    this.value = value;
+    this.unit = unit;
+  }
 }
